28733e0e4d34ef4b9aff704d2e758b319bdc0d85
Tobias Loesche <tobias.loesche@studium.uni-hamburg.de>
2020-03-24 15:35:13 +0100

    Included definitions_analysis.py which needed to be updated for values of training sample.

--- definitions_analysis.py
+++ definitions_analysis.py
@@ -57,8 +57,9 @@ eraDependentParameters = {
         "lumi":  41529.0,
         "lumimask": "data/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt",
         "pu_corrections_file" : "data/pileup_Cert_294927-306462_13TeV_PromptReco_Collisions17_withVar.root",
-        "btag_SF_btagDeepB" : "./data/deepCSV_sfs_v2.btag.csv",
-        "btag_SF_btagCSVV2" : "./data/CSVv2_Moriond17_B_H.btag.csv",
+        "btag_SF_btagDeepB" : "./data/DeepCSV_94XSF_V5_B_F.btag.csv",
+        "btag_SF_btagCSVV2" : "./data/CSVv2_94XSF_V2_B_F_2017.btag.csv",
+        "btag_SF_btagDeepFlavB" : "./data/sfs_deepjet_2017_19-11-11.btag.csv",
         #"BTagSFbtagCSVV2 * ./data/CSVv2_94XSF_V2_B_F_2017.btag.csv",
         #"btag_SF_btagDeepB * ./data/deepCSV_sfs_v2.btag.csv",
         #"BTagSF * ./data/DeepCSV_94XSF_V4_B_F.btag.csv"
@@ -73,10 +74,11 @@ eraDependentParameters = {
             #"BTagSF * ./data/DeepCSV_94XSF_V5_B_F.btag.csv"
             #"BTagSF * ./data/deepCSV_sfs_v2.btag.csv"
         ],
-        "btagging algorithm" : "btagDeepB",
+        "btagging algorithm" : "btagDeepFlavB",
         "btagging WP" : 
             {"btagDeepB": 0.4941, # medium working point for btagDeepB
-            "btagCSVV2": 0.8484 # medium working point for btagCSVV2
+            "btagCSVV2": 0.8484, # medium working point for btagCSVV2
+            "btagDeepFlavB": 0.3033 # medium working point for btagDeepFlavB
             },
         "bbtagging WP" : 0.8, # medium 2 working point for DeepDoubleB tagger
     }
@@ -86,7 +88,7 @@ eraDependentParameters = {
 
 #################################### Samples info ##############################################################################
 
-dataset = "maren_v2"
+dataset = "nanoAODv5_central"
 
 genweights = {
     "maren": {
@@ -110,6 +112,33 @@ genweights = {
         "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8": 283000430.5968169, #not full statistics
         "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8": 1647945788.3386502, #not full statistics
     },
+    "nanoAODv5_central":{
+        "ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8": 4216319.315883999,
+        "ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pythia8": 2215145.8533389997,
+        "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8": 33091176613.77194,
+        "ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8": 4484065.542378,
+        "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8": 4980769113.241218, 
+        "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8": 40821660261.670876, 
+        "ST_s-channel_4f_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8": 37052021.59459843,
+        "ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8": 3675910.0,
+        "ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8": 5982064.0,
+        "ST_tW_antitop_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8": 270762750.1725248,
+        "ST_tW_top_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8": 277241050.84022206,
+        "TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8": 560315.1334199999,
+        "TTWJetsToLNu_TuneCP5_PSweights_13TeV-amcatnloFXFX-madspin-pythia8": 1690120.2450226927,
+        "TTZToQQ_TuneCP5_13TeV-amcatnlo-pythia8": 383062.06864380004,
+        "TTZToLLNuNu_M-10_TuneCP5_PSweights_13TeV-amcatnlo-pythia8": 2694672.7126361188,
+        #"WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8":  29981320.0,
+        "WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8": 11026234235687.748,
+        "WJetsToLNu_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8": 4064208918856.2876,
+        "WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8": 2429846590795.5117,
+        "DYJetsToLL_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8": 533210706325.52826,
+        "DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8": 445782009508.43427,
+        "DYJetsToLL_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8": 142389472522.05267,
+        "WW_TuneCP5_13TeV-pythia8": 7765891.020262634,
+        "WZ_TuneCP5_13TeV-pythia8": 3928630.0,
+        "ZZ_TuneCP5_13TeV-pythia8": 1949768.0,
+    },
 }
 
 samples_info = {
@@ -117,6 +146,11 @@ samples_info = {
             "process": "ttHTobb",
             "XS": 0.2934045,
             "ngen_weight": genweights[dataset]["ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8"],
+            },
+    "ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pythia8": {
+            "process": "ttHTobb",
+            "XS": 0.1289155939,
+            "ngen_weight": genweights[dataset]["ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pythia8"],
             },
     "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8": {
             "XS": 365.45736135,
@@ -135,6 +169,96 @@ samples_info = {
             "XS": 377.9607353256,
             "ngen_weight": genweights[dataset]["TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8"],
             },
+    #"ST_s-channel_4f_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8": {
+    #        "process": "singlet",
+    #        "XS": 3.36,
+    #        "ngen_weight": genweights[dataset]["ST_s-channel_4f_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8"],
+    #        },
+    #"ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8": {
+    #        "process": "singlet",
+    #        "XS": 80.95,
+    #        "ngen_weight": genweights[dataset]["ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8"],
+    #        },
+    #"ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8": {
+    #        "process": "singlet",
+    #        "XS": 136.02,
+    #        "ngen_weight": genweights[dataset]["ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8"],
+    #        },
+    #"ST_tW_antitop_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8": {
+    #        "process": "singlet",
+    #        "XS": 35.85,
+    #        "ngen_weight": genweights[dataset]["ST_tW_antitop_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8"],
+    #        },
+    #"ST_tW_top_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8": {
+    #        "process": "singlet",
+    #        "XS": 35.85,
+    #        "ngen_weight": genweights[dataset]["ST_tW_top_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8"],
+    #        },
+    #"TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8": {
+    #        "process": "ttv",
+    #        "XS": 0.3708,
+    #        "ngen_weight": genweights[dataset]["TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8"],
+    #        },
+    #"TTWJetsToLNu_TuneCP5_PSweights_13TeV-amcatnloFXFX-madspin-pythia8": {
+    #        "process": "ttv",
+    #        "XS": 0.1792,
+    #        "ngen_weight": genweights[dataset]["TTWJetsToLNu_TuneCP5_PSweights_13TeV-amcatnloFXFX-madspin-pythia8"],
+    #        },
+    #"TTZToQQ_TuneCP5_13TeV-amcatnlo-pythia8": {
+    #        "process": "ttv",
+    #        "XS": 0.6012,
+    #        "ngen_weight": genweights[dataset]["TTZToQQ_TuneCP5_13TeV-amcatnlo-pythia8"],
+    #        },
+    #"TTZToLLNuNu_M-10_TuneCP5_PSweights_13TeV-amcatnlo-pythia8": {
+    #        "process": "ttv",
+    #        "XS": 0.2589,
+    #        "ngen_weight": genweights[dataset]["TTZToLLNuNu_M-10_TuneCP5_PSweights_13TeV-amcatnlo-pythia8"],
+    #        },
+    #"WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8": {
+    #        "process": "vjets",
+    #        "XS": 50131.98,
+    #        "ngen_weight": genweights[dataset]["WJetsToLNu_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8"],
+    #        },
+    #"WJetsToLNu_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8": {
+    #        "process": "vjets",
+    #        "XS": 8426.09,
+    #        "ngen_weight": genweights[dataset]["WJetsToLNu_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8"],
+    #        },
+    #"WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8": {
+    #        "process": "vjets",
+    #        "XS": 3172.96,
+    #        "ngen_weight": genweights[dataset]["WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8"],
+    #        },
+    #"DYJetsToLL_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8": {
+    #        "process": "vjets",
+    #        "XS": 4620.52,
+    #        "ngen_weight": genweights[dataset]["DYJetsToLL_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8"],
+    #        },
+    #"DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8": {
+    #        "process": "vjets",
+    #        "XS": 859.59,
+    #        "ngen_weight": genweights[dataset]["DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8"],
+    #        },
+    #"DYJetsToLL_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8": {
+    #        "process": "vjets",
+    #        "XS": 338.26,
+    #        "ngen_weight": genweights[dataset]["DYJetsToLL_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8"],
+    #        },
+    #"WW_TuneCP5_13TeV-pythia8": {
+    #        "process": "diboson",
+    #        "XS": 118.7,
+    #        "ngen_weight": genweights[dataset]["WW_TuneCP5_13TeV-pythia8"],
+    #        },
+    #"WZ_TuneCP5_13TeV-pythia8": {
+    #        "process": "diboson",
+    #        "XS": 65.5443,
+    #        "ngen_weight": genweights[dataset]["WZ_TuneCP5_13TeV-pythia8"],
+    #        },
+    #"ZZ_TuneCP5_13TeV-pythia8": {
+    #        "process": "diboson",
+    #        "XS": 15.8274,
+    #        "ngen_weight": genweights[dataset]["ZZ_TuneCP5_13TeV-pythia8"],
+    #        },
 }
 
 
@@ -150,14 +274,86 @@ histogram_settings = {
     "leading_jet_pt" : (0,500,31),
     "leading_jet_pt_nom" : (0,500,31),
     "leading_jet_eta" : (-2.4,2.4,31),
+    "leading_jet_phi" : (-3.14,3.14,31),
+    "leading_jet_mass" : (0,300,31),
+    "leading_jet_btagDeepFlavB" : (0,1,31),
     "leading_lepton_pt" : (0,500,31),
     "leading_lepton_eta" : (-2.4,2.4,31),
+    "leading_lepton_phi" : (-3.14,3.14,31),
+    "leading_lepton_mass" : (0,300,31),
     "leading_bjet_pt" : (0,500,31),
     "leading_bjet_pt_nom" : (0,500,31),
     "leading_bjet_eta" : (-2.4,2.4,31),
     "subleading_bjet_pt" : (0,500,31),
     "subleading_bjet_pt_nom" : (0,500,31),
     "subleading_bjet_eta" : (-2.4,2.4,31),
+
+    "subleading_jet_pt" : (0,500,31),
+    "subleading_jet_pt_nom" : (0,500,31),
+    "subleading_jet_eta" : (-2.4,2.4,31),
+    "subleading_jet_phi" : (-3.14,3.14,31),
+    "subleading_jet_mass" : (0,300,31),
+    "subleading_jet_btagDeepFlavB" : (0,1,31),
+    
+    "third_jet_pt" : (0,500,31),
+    "third_jet_pt_nom" : (0,500,31),
+    "third_jet_eta" : (-2.4,2.4,31),
+    "third_jet_phi" : (-3.14,3.14,31),
+    "third_jet_mass" : (0,300,31),
+    "third_jet_btagDeepFlavB" : (0,1,31),
+    
+    "fourth_jet_pt" : (0,500,31),
+    "fourth_jet_pt_nom" : (0,500,31),
+    "fourth_jet_eta" : (-2.4,2.4,31),
+    "fourth_jet_phi" : (-3.14,3.14,31),
+    "fourth_jet_mass" : (0,300,31),
+    "fourth_jet_btagDeepFlavB" : (0,1,31),
+    
+    "fifth_jet_pt" : (0,500,31),
+    "fifth_jet_pt_nom" : (0,500,31),
+    "fifth_jet_eta" : (-2.4,2.4,31),
+    "fifth_jet_phi" : (-3.14,3.14,31),
+    "fifth_jet_mass" : (0,300,31),
+    "fifth_jet_btagDeepFlavB" : (0,1,31),
+    
+    "sixth_jet_pt" : (0,500,31),
+    "sixth_jet_pt_nom" : (0,500,31),
+    "sixth_jet_eta" : (-2.4,2.4,31),
+    "sixth_jet_phi" : (-3.14,3.14,31),
+    "sixth_jet_mass" : (0,300,31),
+    "sixth_jet_btagDeepFlavB" : (0,1,31),
+    
+    "seventh_jet_pt" : (0,500,31),
+    "seventh_jet_pt_nom" : (0,500,31),
+    "seventh_jet_eta" : (-2.4,2.4,31),
+    "seventh_jet_phi" : (-3.14,3.14,31),
+    "seventh_jet_mass" : (0,300,31),
+    "seventh_jet_btagDeepFlavB" : (0,1,31),
+    
+    "eighth_jet_pt" : (0,500,31),
+    "eighth_jet_pt_nom" : (0,500,31),
+    "eighth_jet_eta" : (-2.4,2.4,31),
+    "eighth_jet_phi" : (-3.14,3.14,31),
+    "eighth_jet_mass" : (0,300,31),
+    "eighth_jet_btagDeepFlavB" : (0,1,31),
+    
+    "ninth_jet_pt" : (0,500,31),
+    "ninth_jet_pt_nom" : (0,500,31),
+    "ninth_jet_eta" : (-2.4,2.4,31),
+    "ninth_jet_phi" : (-3.14,3.14,31),
+    "ninth_jet_mass" : (0,300,31),
+    "ninth_jet_btagDeepFlavB" : (0,1,31),
+    
+    "tenth_jet_pt" : (0,500,31),
+    "tenth_jet_pt_nom" : (0,500,31),
+    "tenth_jet_eta" : (-2.4,2.4,31),
+    "tenth_jet_phi" : (-3.14,3.14,31),
+    "tenth_jet_mass" : (0,300,31),
+    "tenth_jet_btagDeepFlavB" : (0,1,31),
+    
+    "MET_pt" : (0,500,31),
+    "MET_phi" : (-3.14,3.14,31),
+    "MET_sumEt" : (0, 300,31),
 
     "higgs_pt": (0,500,31),
     "higgs_eta": (-2.4,2.4,31),
